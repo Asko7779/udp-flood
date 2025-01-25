90abb28b8d53a969548fdeb1069e4c2fe506a1c5
Asko7779 <169293034+Asko7779@users.noreply.github.com>
2025-01-25 14:31:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <winsock2.h>
 #include <iostream>
-
 #pragma comment(lib, "ws2_32.lib")
 
 int main() {
@@ -13,14 +12,19 @@ int main() {
     sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
     target.sin_family = AF_INET;
-    target.sin_port = htons(8080);
+
+    // target port and IP, can be adjusted
+    
+    target.sin_port = htons(8080);                      
     target.sin_addr.s_addr = inet_addr("127.0.0.1");
-    memset(buffer, 'A', sizeof(buffer));
-    for (int i = 0; i < 550000; i++) {
+    
+    memset(buffer, 'A', sizeof(buffer));    // fill up the packet
+    for (int i = 0; i < 100; i++) {         // sends 100 packets, also adjustable
         sendto(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&target, sizeof(target));
         std::cout << "Sent packet to " << target.sin_addr.s_addr << std::endl;
     }
 
+    // socket clearing
     closesocket(sock);
     WSACleanup();
     return 0;
